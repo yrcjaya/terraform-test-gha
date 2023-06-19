06672d2419ea8ff4b2699cad4956bb93a6756d93
Jayaseelan Yezhuaralai <yrcjaya@gmail.com>
2023-06-19 22:34:18 +0800

    Hello World Terraform

---
+++ main.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_version = ">= 1.4.6"
+}
+
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+output "hello_world" {
+  value = "Hello, World!"
+}
